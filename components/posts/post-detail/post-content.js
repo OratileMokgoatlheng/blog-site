c11e86103b896f46e69b15eef20604bf53bb714e
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-09-20 18:51:11 +0200

    showingImageUsingMarkdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,18 +1,50 @@
-import ReactMarkdown from "react-markdown"
-import PostHeader from "./post-header"
+import ReactMarkdown from "react-markdown";
+import PostHeader from "./post-header";
+import Image from "next/image";
 
-import classes from './post-content.module.css'
+import classes from "./post-content.module.css";
 
-function PostContent(props){
-const {post} = props
-    const imagePath = `/images/posts/${post.slug}/${post.image}`
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
 
-    return(
-        <article className={classes.content}> 
-            <PostHeader title={post.title} image={imagePath} />
-            <ReactMarkdown>{post.content}</ReactMarkdown>
-        </article>
-    )
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>
+    },
+  };
+  console.log('images: ', customRenderers)
+  return (
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
+    </article>
+  );
 }
 
-export default PostContent
+export default PostContent;
