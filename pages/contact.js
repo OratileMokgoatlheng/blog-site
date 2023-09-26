ddf2f39fc5fdba744c4ebc824950a9bc06a6ca90
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-09-26 09:58:42 +0200

    addedHeadTags

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
 import ContactForm from "../components/contact/contact-form";
 
 function ContactPage() {
-  return <ContactForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Send me your messages!" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 }
 
 export default ContactPage;
